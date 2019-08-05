7f1941cd0ffe295f075f55a437942d754f05f0ae
Stuart Kent <skent.dev@gmail.com>
2019-08-05 14:38:56 -0400

    Add shell iOS app

---
+++ part2-refactor/SpeedySubs/SpeedySubs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedySubs
+//
+//  Created by Stuart Kent on 8/5/19.
+//  Copyright © 2019 Stuart Kent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
