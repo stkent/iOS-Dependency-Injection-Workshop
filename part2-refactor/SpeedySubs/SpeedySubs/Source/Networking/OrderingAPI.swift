1a32dacaf7e7148b254d4114fb40a61c2bd7e003
Stuart Kent <skent.dev@gmail.com>
2019-08-09 10:21:01 -0400

    Use result type in networking callbacks

--- part2-refactor/SpeedySubs/SpeedySubs/Source/Networking/OrderingAPI.swift
+++ part2-refactor/SpeedySubs/SpeedySubs/Source/Networking/OrderingAPI.swift
@@ -2,11 +2,16 @@ import Foundation
 
 private let networkCallDuration = DispatchTimeInterval.seconds(2)
 
+enum NetworkingError: Error {
+    case noInternet
+    case serverDown
+}
+
 class OrderingAPI {
 
     func logIn(username: String,
                password: String,
-               completionHandler: @escaping (Customer) -> ()) {
+               completionHandler: @escaping (Result<Customer, NetworkingError>) -> ()) {
 
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + networkCallDuration) {
             let creditCards: [CreditCard] = [
@@ -15,11 +20,11 @@ class OrderingAPI {
                 CreditCard(id: 3, displayName: "Visa 3333", expirationDate: DateComponents(year: 2025, month: 4, day: 30))
             ]
 
-            completionHandler(Customer(id: UUID.init().uuidString, creditCards: creditCards))
+            completionHandler(.success(Customer(id: UUID.init().uuidString, creditCards: creditCards)))
         }
     }
 
-    func getCustomerCreditCards(completionHandler: @escaping ([CreditCard]) -> ()) {
+    func getCustomerCreditCards(completionHandler: @escaping (Result<[CreditCard], NetworkingError>) -> ()) {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + networkCallDuration) {
             let creditCards: [CreditCard] = [
                 CreditCard(id: 1, displayName: "Visa 1111", expirationDate: DateComponents(year: 2020, month: 10, day: 31)),
@@ -28,11 +33,11 @@ class OrderingAPI {
                 CreditCard(id: 4, displayName: "Amex 4444", expirationDate: DateComponents(year: 2019, month: 12, day: 31))
             ]
 
-            completionHandler(creditCards)
+            completionHandler(.success(creditCards))
         }
     }
 
-    func getSandwiches(completionHandler: @escaping ([Sandwich]) -> ()) {
+    func getSandwiches(completionHandler: @escaping (Result<[Sandwich], NetworkingError>) -> ()) {
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + networkCallDuration) {
             let sandwiches = [
                 Sandwich(id: 10, name: "BLT"),
@@ -42,16 +47,16 @@ class OrderingAPI {
                 Sandwich(id: 50, name: "Everything")
             ]
 
-            completionHandler(sandwiches)
+            completionHandler(.success(sandwiches))
         }
     }
 
     func placeOrder(customer: Customer,
                     order: Order,
-                    completionHandler: @escaping (Int) -> ()) {
+                    completionHandler: @escaping (Result<Int, NetworkingError>) -> ()) {
 
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + networkCallDuration) {
-            completionHandler(Int.random(in: Int.min...Int.max))
+            completionHandler(.success(Int.random(in: Int.min...Int.max)))
         }
     }
 
